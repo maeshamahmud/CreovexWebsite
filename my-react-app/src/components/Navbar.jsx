d3fd4bbd91f1a41e800b42e6af0008f97af0607e
IcedCaffeine <fahad7923@gmail.com>
2025-01-05 17:27:59 -0500

    navbar and hero section woth bootstrap

---
+++ my-react-app/src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+
+const NavbarC = () => {
+  return (
+    // <nav>
+    //   <h1>Creovex</h1>
+    //   <div>
+    //     <a href="#">Home </a>
+    //     <a href="#">Case Studies </a>
+    //     <a href="#">Blog </a>
+    //     <a href="#">About us </a>
+    //     <button>Book a call </button>
+    //   </div>
+    // </nav>
+    <Container>
+    <Navbar expand="lg" className="bg-body-tertiary" fixed = "top">
+      <Container>
+        <Navbar.Brand href="#">Creovex</Navbar.Brand>
+      </Container>
+    </Navbar>
+  </Container>
+
+
+  );
+};
+
+export default NavbarC;
+
